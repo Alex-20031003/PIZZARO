849758eb92327548c911cd8ca0c16feb50cabd5f
Алексей Руденок <oleksiirudenok@gmail.com>
2026-04-15 17:38:36 +0200

    fix: delete props width and height in Image UI element

--- src/shared/ui/Image.tsx
+++ src/shared/ui/Image.tsx
@@ -2,11 +2,9 @@ type ImageProps = {
   src: string
   title?: string
   alt: string
-  width?: number | string
-  height?: number | string
   className?: string
 }
 
-export default function Image({ src, title, alt, width, height, className }: ImageProps) {
-  return <img src={src} width={width} height={height} alt={alt} title={title} className={`block max-w-full h-auto ${className}`} />
+export default function Image({ src, title, alt, className }: ImageProps) {
+  return <img src={src} alt={alt} title={title} className={`block max-w-full h-auto ${className}`} />
 }

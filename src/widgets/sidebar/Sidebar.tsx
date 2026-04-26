b55147580e02b62cfaa8329d4ba44545d5322698
Алексей Руденок <oleksiirudenok@gmail.com>
2026-04-26 11:33:10 +0200

    styles: add some styles for Sidebar

--- src/widgets/sidebar/Sidebar.tsx
+++ src/widgets/sidebar/Sidebar.tsx
@@ -16,10 +16,14 @@ export default function Sidebar({ activeSidebar, onClose }: SidebarProps) {
         onClick={onClose}
       />
 
+<<<<<<< Updated upstream
       <aside
         className={`fixed top-0 right-0 z-50 h-full w-full max-w-105 bg-white shadow-xl transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'
           }`}
       >
+=======
+      <aside className={`fixed top-0 right-0 z-50 h-full w-full max-w-full sm:max-w-105 py-6 px-10 bg-white shadow-xl transition-translate duration-400 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+>>>>>>> Stashed changes
         <button type="button" onClick={onClose}>
           Close
         </button>

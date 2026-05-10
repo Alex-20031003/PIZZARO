1f39a7f4cb81e8d52e702fe3846ac60fd24847ee
Алексей Руденок <oleksiirudenok@gmail.com>
2026-05-10 18:27:53 +0200

    feat: create UI-Element Card Box

---
+++ src/shared/ui/CardBox.tsx
@@ -0,0 +1,11 @@
+type CardBoxProps = {
+  children: React.ReactNode
+}
+
+export default function CardBox({ children }: CardBoxProps) {
+  return (
+    <div className='bg-(--light-grey) rounded-lg max-w-70 w-full'>
+      {children}
+    </div>
+  )
+}

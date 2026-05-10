3d81ac7802c764d4f5dd1672b139f77dba652428
Алексей Руденок <oleksiirudenok@gmail.com>
2026-05-10 22:06:36 +0200

    feat: create categories block for Home Page

--- src/pages/home/Categories.tsx
+++ src/pages/home/Categories.tsx
@@ -1,13 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCategories } from '@/entities/categories/api/categoriesApi';
 import CardBox from '@/shared/ui/CardBox';
+import Container from '@/shared/ui/Container';
 import { ChevronRight } from 'lucide-react';
+import { Link } from 'react-router';
 
 
 
 export default function Categories() {
   const {
-    data: categories = [],
+    data: categories,
     isLoading,
     isError,
     error,
@@ -25,18 +27,22 @@ export default function Categories() {
   }
 
   return (
-    <section>
-      <h2>Explorer Our Menu</h2><ul>
-        {categories.map(category => (
-          <CardBox key={category.id}>
-            <img src={category.image_url} alt={category.title} />
-            <div>
-              <h3>{category.title}</h3>
-              <ChevronRight />
-            </div>
-          </CardBox>
-        ))}
-      </ul>
+    <section className='flex flex-col items-center mt-9 mb-9'>
+      <Container>
+        <h2 className='text-4xl font-semibold mb-9 text-center'>Explorer Our Menu</h2>
+        <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center justify-between gap-6'>
+          {categories.map(category => (
+            <CardBox key={category.id}>
+              <div className='relative flex flex-col items-center justify-center pb-3'>
+                <img src={category.image_url} alt={category.title} className='rounded-xl' />
+                <h3 className='text-2xl font-bold'>{category.title}</h3>
+                <ChevronRight className='absolute right-3 bottom-3 size-8 text-[#F05A24]' strokeWidth={2.5} />
+              </div>
+            </CardBox>
+          ))}
+        </ul>
+      </Container>
+
     </section>
   )
 }

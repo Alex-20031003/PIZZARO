308e54b0a5b84bd4038810f6dc6147e3c0d459ff
Алексей Руденок <oleksiirudenok@gmail.com>
2026-05-12 20:33:55 +0200

    feat: Replace category block content with local file loading

--- src/entities/categories/api/categoriesApi.ts
+++ src/entities/categories/api/categoriesApi.ts
@@ -1,15 +1,15 @@
 import { supabase } from '@/shared/api/supabase/client'
-import type { Category } from '../model/types.data'
+import type { Category } from '../model/types'
 
 export async function getCategories(): Promise<Category[]> {
   const { data, error } = await supabase
     .from('categories')
     .select('id, title, slug, image_url')
     .order('id', { ascending: true })
-  
+
   if (error) {
     throw new Error(error.message)
   }
-  
+
   return data as Category[]
 }
--- src/widgets/categories/Categories.tsx
+++ src/widgets/categories/Categories.tsx
@@ -1,30 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
-import { getCategories } from '@/entities/categories/api/categoriesApi';
 import Container from '@/shared/ui/Container';
-import CardCategory from '@/entities/categories/ui/CardCategory';
-import CategoriesSkeleton from './CategoriesSkeleton';
-import CategoriesError from './CategoriesError';
+import CardList from '@/entities/categories/ui/CardList';
 
 export default function Categories() {
-  const {
-    data: categories,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ['categories'],
-    queryFn: getCategories,
-    staleTime: 10 * 60 * 1000,
-  })
-
-  if (isLoading) return <CategoriesSkeleton />
-
-  if (isError) return <CategoriesError error={(error as Error).message} />
   return (
     <section className='mt-9 mb-9'>
       <Container className='flex flex-col'>
         <h2 className='text-4xl font-semibold mb-9 text-center'>Explorer Our Menu</h2>
-        <CardCategory categories={categories} />
+        <CardList />
       </Container>
     </section>
   )

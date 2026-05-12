2cb05d311ee35fe01e135655bff498cf653a6246
Алексей Руденок <oleksiirudenok@gmail.com>
2026-05-12 11:06:07 +0200

    feat: add z-index for backdrop

--- src/widgets/sidebar/Sidebar.tsx
+++ src/widgets/sidebar/Sidebar.tsx
@@ -11,19 +11,15 @@ export default function Sidebar({ activeSidebar, onClose }: SidebarProps) {
   return (
     <>
       <div
-        className={`fixed inset-0 bg-black/40 transition-opacity duration-300 ${isOpen ? 'visible opacity-100' : 'invisible opacity-0'
+        className={`fixed inset-0 z-50 bg-black/40 transition-opacity duration-300 ${isOpen ? 'visible opacity-100' : 'invisible opacity-0'
           }`}
         onClick={onClose}
       />
 
-<<<<<<< Updated upstream
       <aside
         className={`fixed top-0 right-0 z-50 h-full w-full max-w-105 bg-white shadow-xl transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'
           }`}
       >
-=======
-      <aside className={`fixed top-0 right-0 z-50 h-full w-full max-w-full sm:max-w-105 py-6 px-10 bg-white shadow-xl transition-translate duration-400 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
->>>>>>> Stashed changes
         <button type="button" onClick={onClose}>
           Close
         </button>

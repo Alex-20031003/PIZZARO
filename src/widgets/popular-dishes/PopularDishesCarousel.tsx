259a23723f6f76a152e4f887baa5711902b9f431
Алексей Руденок <oleksiirudenok@gmail.com>
2026-05-20 15:11:25 +0200

    feat: сreating a function to control the carousel via the keyboard

--- src/widgets/popular-dishes/PopularDishesCarousel.tsx
+++ src/widgets/popular-dishes/PopularDishesCarousel.tsx
@@ -58,6 +58,35 @@ export function PopularDishesCarousel({ products }: PopularDishesCarouselProps) 
     setSelectedIndex(emblaApi.selectedScrollSnap())
   }, [emblaApi])
 
+  const handleManualScroll = useCallback((action: () => void) => {
+    action()
+
+    autoplay.current.stop()
+
+    if(isInView && !isPause){
+      autoplay.current.play()
+    }
+  }, [isInView, isPause])
+
+  useEffect(() => {
+    const handleArrowScroll = (e: KeyboardEvent) => {
+      if (!isInView) return
+
+      if (e.key === 'ArrowLeft') {
+        e.preventDefault()
+        handleManualScroll(scrollPrev)
+      } else if (e.key === 'ArrowRight'){
+        e.preventDefault()
+        handleManualScroll(scrollNext)
+      }
+    }
+
+    document.addEventListener('keydown', handleArrowScroll)
+
+    return () => { document.removeEventListener('keydown', handleArrowScroll) }
+
+  }, [isInView, scrollNext, scrollPrev, handleManualScroll])
+
   useEffect(() => {
     if (!emblaApi) return
 
@@ -85,12 +114,12 @@ export function PopularDishesCarousel({ products }: PopularDishesCarouselProps) 
 
   if (!products.length) return null
 
-  const handelMouseEnter = () => {
+  const handleMouseEnter = () => {
     setIsPause(true)
     autoplay.current.stop()
   }
 
-  const handelMouseLeave = () => {
+  const handleMouseLeave = () => {
     setIsPause(false)
 
     if (isInView) {
@@ -99,20 +128,20 @@ export function PopularDishesCarousel({ products }: PopularDishesCarouselProps) 
   }
 
   return (
-    <div ref={carouselVisibilityRef} className='relative'>
+    <div ref={carouselVisibilityRef} className='relative outline-none'>
       <div className='overflow-hidden' ref={emblaRef}>
-        <ul className='flex' onMouseEnter={handelMouseEnter} onMouseLeave={handelMouseLeave}>
+        <ul className='flex' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
           {products.map((product) => (
             <ProductCard key={product.id} product={product} classNameCardBox='ml-6 shrink-0 basis-full min-w-20 md:basis-[calc((100%_-_24px)_/_2)] xl:basis-[calc((100%_-_72px)_/_4)]' />
           ))}
         </ul>
       </div>
 
-      <button type='button' onClick={scrollPrev} className='absolute -left-7.5 top-1/2 z-10 -translate-y-1/2 w-10 sm:w-15 h-auto bg-(--light-grey) shadow-[0_0_5px_rgba(0,0,0,0.25)] rounded-full'>
+      <button type='button' onClick={() => handleManualScroll(scrollPrev)} className='absolute -left-7.5 top-1/2 z-10 -translate-y-1/2 w-10 sm:w-15 h-auto bg-(--light-grey) shadow-[0_0_5px_rgba(0,0,0,0.25)] rounded-full'>
         <ChevronLeft color='#F05A24' className='w-full h-full' />
       </button>
 
-      <button type='button' onClick={scrollNext} className='absolute -right-7.5 top-1/2 z-10 -translate-y-1/2 w-10 sm:w-15 h-auto bg-(--light-grey) shadow-[0_0_5px_rgba(0,0,0,0.25)] rounded-full'>
+      <button type='button' onClick={() => handleManualScroll(scrollNext)} className='absolute -right-7.5 top-1/2 z-10 -translate-y-1/2 w-10 sm:w-15 h-auto bg-(--light-grey) shadow-[0_0_5px_rgba(0,0,0,0.25)] rounded-full'>
         <ChevronRight color='#F05A24' className='w-full h-full' />
       </button>
 
@@ -124,7 +153,7 @@ export function PopularDishesCarousel({ products }: PopularDishesCarouselProps) 
             <button
               key={index}
               type="button"
-              onClick={() => scrollTo(index)}
+              onClick={() => handleManualScroll(() => scrollTo(index))}
               className={`relative h-3 overflow-hidden rounded-full bg-black/20 transition-all duration-300 ${isActive ? 'w-8' : 'w-4'
                 }`}
             >

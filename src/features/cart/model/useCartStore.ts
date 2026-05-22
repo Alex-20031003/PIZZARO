accae44057f3e91ef50127c5d34841b12a6de943
Алексей Руденок <oleksiirudenok@gmail.com>
2026-05-22 22:13:43 +0200

    feat: create useCartStore hook

---
+++ src/features/cart/model/useCartStore.ts
@@ -0,0 +1,43 @@
+import type { CartItem } from './types'
+import { create } from 'zustand'
+
+interface CartState {
+  items: CartItem[]
+  addItem: (item: Omit<CartItem, 'quantity'>) => void
+  removeItem: (id: number) => void
+  increaseQuantity: (id: number) => void
+  decreaseQuantity: (id: number) => void
+  clearCart: () => void
+}
+
+export const useCartStore = create<CartState>((set) => ({
+  items: [],
+
+  addItem: (item) => set((state) => {
+    const existingItem = state.items.find((cartItem) => cartItem.id === item.id)
+
+    if (existingItem) {
+      return {
+        items: state.items.map((cartItem) => cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem)
+      }
+    }
+
+    return {
+      items: [...state.items, { ...item, quantity: 1 }]
+    }
+  }),
+
+  removeItem: (id) => set((state) => ({
+    items: state.items.filter((item) => item.id !== id)
+  })),
+
+  increaseQuantity: (id) => set((state) => ({
+    items: state.items.map((item) => item.id === id ? { ...item, quantity: item.quantity + 1 } : item)
+  })),
+
+  decreaseQuantity: (id) => set((state) => ({
+    items: state.items.map((item) => item.id === id ? { ...item, quantity: item.quantity - 1 } : item).filter((item) => item.quantity > 0)
+  })),
+
+  clearCart: () => set({ items: [] })
+}))

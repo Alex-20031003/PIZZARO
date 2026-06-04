742205df8bd3ace1bd4d7668e0feccc0b59f31c9
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-04 17:01:58 +0200

    styles: add and change styles for Contact Card

--- src/widgets/contact-card/ContactCard.tsx
+++ src/widgets/contact-card/ContactCard.tsx
@@ -9,17 +9,17 @@ interface ContactCardProps {
 
 export default function ContactCard({ icon: Icon, headline, text }: ContactCardProps) {
   return (
-    <li className='bg-white py-15 flex flex-col items-center gap-6 max-w-100 w-full rounded-lg shadow-[5px_0_20px_rgba(0,0,0,0.4)]'>
+    <li className='bg-white flex flex-col items-center justify-center gap-6 2xl:max-w-120 max-w-90 sm:h-80 h-70 w-full rounded-lg shadow-[5px_0_20px_rgba(0,0,0,0.4)]'>
       <Icon stroke='#F05A24' size={64} />
-      <h2 className='text-5xl tracking-[2%] font-medium'>{headline}</h2>
-      { text && (<p className='text-2xl font-medium text-[#6b6b6b] hover:text-(--primary) transition-colors duration-300 ease-in-out cursor-pointer'>{text}</p> )}
+      <h2 className='2xl:text-5xl xl:text-3xl sm:text-5xl text-3xl  tracking-[2%] font-medium'>{headline}</h2>
+      { text && (<p className='2xl:text-2xl xl:text-xl sm:text-2xl text-base font-medium text-[#6b6b6b] hover:text-(--primary) transition-colors duration-300 ease-in-out cursor-pointer'>{text}</p> )}
 
       {!text && (
-        <ul className='flex flex-row w-full items-center justify-between px-25'>
+        <ul className='flex flex-row w-full items-center justify-center sm:gap-15 gap-10 mx-auto'>
           {SOCIAL_ITEMS.map(({ to, icon: Icon, label }) => (
             <li key={to}>
               <a href={to} aria-label={label} target="_blank" rel='noopener noreferrer' className='text-[#6b6b6b] hover:text-(--primary) transition-colors duration-300'>
-                <Icon className='w-8 h-8' />
+                <Icon className='2xl:w-8 2xl:h-8 w-6 h-6' />
               </a>
             </li>
           ))}

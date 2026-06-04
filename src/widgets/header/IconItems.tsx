06cb19e694a51e52b4a36399336a3793a5269455
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-04 18:20:47 +0200

    feat: add logic for count in cart for header

--- src/widgets/header/IconItems.tsx
+++ src/widgets/header/IconItems.tsx
@@ -2,22 +2,31 @@ import { Heart, ShoppingBag, User, Menu } from 'lucide-react'
 import { useCartStore } from '@/features/cart/model/useCartStore'
 
 
-type IconsProps= {
+type IconsProps = {
   onOpenCart: () => void
   onOpenFavorite: () => void
   onOpenUser: () => void
   onOpenNavigation: () => void
 }
 
-export default function IconItems({onOpenCart, onOpenFavorite, onOpenUser, onOpenNavigation}: IconsProps) {
+export default function IconItems({ onOpenCart, onOpenFavorite, onOpenUser, onOpenNavigation }: IconsProps) {
   const totalCountCart = useCartStore((state) => state.getTotalCartCount())
 
   return (
     <div className='flex flex-row items-center justify-end gap-6'>
       <Heart onClick={onOpenFavorite} size={24} className='cursor-pointer hidden sm:block' />
-      <ShoppingBag onClick={onOpenCart} size={24} className='relative cursor-pointer hidden sm:block before:content-[""] before:absolute before:top-0 before:right-0 before:w-4 before:h-4 before:bg-(--primary) before:rounded-full before:flex before:items-center before:justify-center before:text-white before:text-xs before:font-bold'>
-        { totalCountCart }
-      </ShoppingBag>
+      <button
+        onClick={onOpenCart}
+        className='relative cursor-pointer hidden sm:block'
+      >
+        <ShoppingBag size={24} />
+
+        {totalCountCart >= 0 && (
+          <span className='absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-(--primary) text-xs text-white'>
+            { totalCountCart > 5 ? '5+' : totalCountCart }
+          </span>
+        )}
+      </button>
       <User onClick={onOpenUser} size={24} className='cursor-pointer hidden sm:block' />
       <Menu size={24} onClick={onOpenNavigation} className='sm:hidden' />
     </div>

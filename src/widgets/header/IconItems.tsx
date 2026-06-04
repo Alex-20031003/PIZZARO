8ee7209c6b04e353e344bd61b49642e36105b968
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-04 18:43:26 +0200

    feat: add favorites counter badge

--- src/widgets/header/IconItems.tsx
+++ src/widgets/header/IconItems.tsx
@@ -1,5 +1,6 @@
 import { Heart, ShoppingBag, User, Menu } from 'lucide-react'
 import { useCartStore } from '@/features/cart/model/useCartStore'
+import { useFavoriteStore } from '@/features/favorite/model/useFavoriteStore'
 
 
 type IconsProps = {
@@ -11,19 +12,29 @@ type IconsProps = {
 
 export default function IconItems({ onOpenCart, onOpenFavorite, onOpenUser, onOpenNavigation }: IconsProps) {
   const totalCountCart = useCartStore((state) => state.getTotalCartCount())
+  const totalCountFavorites = useFavoriteStore((state) => state.getTotalFavoritesCount())
 
   return (
     <div className='flex flex-row items-center justify-end gap-6'>
-      <Heart onClick={onOpenFavorite} size={24} className='cursor-pointer hidden sm:block' />
+      <button
+        onClick={onOpenFavorite}
+        className='relative cursor-pointer hidden sm:block'
+        >
+        <Heart onClick={onOpenFavorite} size={24} className='cursor-pointer hidden sm:block' />
+
+        {totalCountFavorites > 0 && (
+          <span className='absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-(--primary) text-xs text-white'>{ totalCountFavorites }</span>
+        )}
+      </button>
       <button
         onClick={onOpenCart}
         className='relative cursor-pointer hidden sm:block'
       >
         <ShoppingBag size={24} />
 
-        {totalCountCart >= 0 && (
+        {totalCountCart > 0 && (
           <span className='absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-(--primary) text-xs text-white'>
-            { totalCountCart > 5 ? '5+' : totalCountCart }
+            {totalCountCart > 5 ? '5+' : totalCountCart}
           </span>
         )}
       </button>

0668b93a2244e18f6cd695303e932da1b12dc55f
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-05 21:37:07 +0200

    fix: reset scroll position on page navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
 import AppRouter from './router/AppRouter';
+import ScrollToTop from './../shared/lib/ScrollToTop';
 
 export default function App() {
-  return <AppRouter />
+  return( 
+  <>
+    <ScrollToTop />
+    <AppRouter />  
+  </> 
+  )
 }

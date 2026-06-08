125938f8f93b3974f216ccc6c5c9774cc8273965
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-08 21:14:48 +0200

    feat: add components for menu aside

--- src/pages/menu/MenuPage.tsx
+++ src/pages/menu/MenuPage.tsx
@@ -1,5 +1,18 @@
+import Container from '@/shared/ui/Container';
+import MenuCategoriesNavigation from '@/widgets/menu-navigation/MenuCategoriesNavigation';
+import Search from '@/widgets/search/Search';
+
 export default function MenuPage() {
   return (
-    <div>MenuPage</div>
+    <section className='mt-19 mb-32'>
+      <Container className='flex flex-row items-start justify-between'>
+        <aside className='flex flex-col gap-9 max-w-80 w-full'>
+          <Search />
+          <MenuCategoriesNavigation />
+        </aside>
+
+        <div className='w-200 h-200 bg-amber-600 flex-1' />
+      </Container>
+    </section>
   )
 }

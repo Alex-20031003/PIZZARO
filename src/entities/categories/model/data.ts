19962486c7aa01b64ef909a7a7ff93a230206b89
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-12 08:02:41 +0200

    rename a categories data file

---
+++ src/entities/categories/model/data.ts
@@ -0,0 +1,71 @@
+import pizza from '@/assets/categories/pizza_pepperoni.webp';
+import sushi from '@/assets/categories/sushi_mixset.webp';
+import burger from '@/assets/categories/burger_bbq.webp';
+import drink from '@/assets/categories/drink_cola.webp';
+import allIcon from '@/assets/categories/allSvg.svg?react'
+import pizzaIcon from '@/assets/categories/pizzaSvg.svg?react'
+import sushiIcon from '@/assets/categories/sushiSvg.svg?react'
+import burgerIcon from '@/assets/categories/burgerSvg.svg?react'
+import drinkIcon from '@/assets/categories/drinkSvg.svg?react'
+import type { Category, categoryMenu } from './types';
+
+
+export const categories: Category[] = [
+  {
+     id: 1, 
+     title: 'Pizza',
+     slug: 'pizza',
+     image_url: pizza
+  },
+  {
+    id: 2,
+    title: 'Sushi',
+    slug: 'sushi',
+    image_url: sushi
+  },
+  {
+    id: 3,
+    title: 'Burger',
+    slug: 'burger',
+    image_url: burger
+  },
+  {
+    id: 4,
+    title: 'Drink',
+    slug: 'drink',
+    image_url: drink
+  }
+]
+
+export const categoriesMenu: categoryMenu[] = [
+  {
+    id: 1,
+    title: 'All',
+    slug: 'all',
+    icon: allIcon
+  },
+  {
+    id: 2, 
+    title: 'Pizza',
+    slug: 'pizza',
+    icon: pizzaIcon
+  },
+  {
+    id: 3, 
+    title: 'Sushi',
+    slug: 'sushi',
+    icon: sushiIcon
+  },
+  {
+    id: 4,
+    title: 'Burgers',
+    slug: 'burger',
+    icon: burgerIcon
+  },
+  {
+    id: 5,
+    title: 'Drinks',
+    slug: 'drink',
+    icon: drinkIcon
+  }
+] 

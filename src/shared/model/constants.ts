280056b0737f83f1a4beb311c9aa59ca905f781e
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-16 17:54:22 +0200

    refactor: reuse shared navigation data

---
+++ src/shared/model/constants.ts
@@ -0,0 +1,9 @@
+import inst from '@/assets/social-icons/inst-logo.svg?react'
+import facebook from '@/assets/social-icons/facebook-logo.svg?react'
+import tiktok from '@/assets/social-icons/tiktok-logo.svg?react'
+
+export const SOCIAL_ITEMS = [
+  { to: 'https://instagram.com/', icon: inst, label: 'Instagram' },
+  { to: 'https://facebook.com/', icon: facebook, label: 'Facebook' },
+  { to: 'https://tiktok.com/', icon: tiktok, label: 'Twitter' }
+]

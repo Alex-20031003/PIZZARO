cc8e274722347a7c892de4214477c132b3ace979
Алексей Руденок <oleksiirudenok@gmail.com>
2026-06-23 16:34:01 +0200

    feat: link Products List to Menu Page

--- src/pages/menu/MenuPage.tsx
+++ src/pages/menu/MenuPage.tsx
@@ -1,8 +1,14 @@
+import type { ProductCategorySlug } from '@/entities/product';
 import Container from '@/shared/ui/Container';
 import MenuCategoriesNavigation from '@/widgets/menu-navigation/MenuCategoriesNavigation';
+import { ProductsList } from '@/widgets/product-list';
 import Search from '@/widgets/search/Search';
+import { useParams } from 'react-router';
 
 export default function MenuPage() {
+  const { categorySlug } = useParams()
+  
+  
   return (
     <section className='mt-19 mb-32'>
       <Container className='flex flex-row items-start justify-between'>
@@ -11,7 +17,11 @@ export default function MenuPage() {
           <MenuCategoriesNavigation />
         </aside>
 
-        <div className='w-200 h-200 bg-amber-600 flex-1' />
+        <div className='flex-1'>
+          <ul className='list-none'>
+            <ProductsList categorySlug={categorySlug as ProductCategorySlug} />
+          </ul>
+        </div>
       </Container>
     </section>
   )

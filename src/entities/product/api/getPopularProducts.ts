7dffd0b08fbfd1cfe4d1a05dbadca690d0563279
Алексей Руденок <oleksiirudenok@gmail.com>
2026-08-07 16:40:15 +0200

    refactor(product): format popular products query

--- src/entities/product/api/getPopularProducts.ts
+++ src/entities/product/api/getPopularProducts.ts
@@ -3,15 +3,15 @@ import type { ProductCardData } from '../model/types';
 
 
 export default async function getPopularProducts(): Promise<ProductCardData[]> {
-  const  { data, error } = await supabase
+  const { data, error } = await supabase
     .from('products')
     .select('id, title, category, slug, ingredients, image_url, base_price, discount_price, rating')
     .eq('is_active', true)
     .eq('is_available', true)
-    .order('rating', { ascending: false})
+    .order('rating', { ascending: false })
     .limit(8)
 
-    if (error) throw new Error(error.message)
+  if (error) throw new Error(error.message)
 
-    return data as ProductCardData[] ?? []
+  return data as ProductCardData[] ?? []
 }

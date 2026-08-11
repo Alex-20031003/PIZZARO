75cfc1cd92d1110858f592f5fc93c8ac7a518934
Алексей Руденок <oleksiirudenok@gmail.com>
2026-08-11 21:04:23 +0200

    feat(auth): add account sidebar states

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
 export { default as AuthProvider } from './model/AuthProvider'
 export { default as useAuth } from './model/useAuth'
+export { default as UserSidebarContent } from './ui/UserSidebarContent'
 export type { AuthContextValue } from './model/AuthContext'
